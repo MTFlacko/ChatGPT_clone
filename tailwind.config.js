10ab4ac77eff2caf85d72b3529f6a959b919e107
MTFlacko <mateusfranc2@gmail.com>
2023-10-25 12:08:48 -0300

    ChatGpt

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+  // Or if using `src` directory:
+  "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  
+],
+darkMode: 'class',
+
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          700:'#40414F',
+          800:'#343541',
+          900:'#202123',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
